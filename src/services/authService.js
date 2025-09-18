aea5d54fcd3c282b4f20284350111baaeb476b5a
veeramallasarvani <2300030714cseh1@gmail.com>
2025-09-18 16:01:59 +0530

    first

---
+++ src/services/authService.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const API_URL = "http://13.232.1.233:8083/back1/auth";
+
+export const login = async (username, password) => {
+  const response = await axios.post(`${API_URL}/login`, { username, password });
+  localStorage.setItem("token", response.data);
+  return response.data;
+};
+
+export const signup = async (username, email, password) => {
+  return axios.post(`${API_URL}/signup`, { username, email, password });
+};
+
+export const logout = () => {
+  localStorage.removeItem("token");
+};
